e8e906ae4a3273889371bd3c3d436df435902778
Michael Maier <mm263@hdm-stuttgart.de>
2025-03-18 10:28:27 +0100

    write-message component added in new folder shared

---
+++ src/app/shared/write-message/write-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-write-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './write-message.component.html',
+  styleUrl: './write-message.component.scss'
+})
+export class WriteMessageComponent {
+
+}

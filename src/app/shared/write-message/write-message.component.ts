8e4db6bd3aa07479bb6364485f07b098abe28454
Michael Maier <mm263@hdm-stuttgart.de>
2025-03-18 11:17:41 +0100

    tag-container added

--- src/app/shared/write-message/write-message.component.ts
+++ src/app/shared/write-message/write-message.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-write-message',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './write-message.component.html',
   styleUrl: './write-message.component.scss'
 })
 export class WriteMessageComponent {
+  exampleUsers = ["Frederik Beck (Du)", "Sofia Müler", "Noah Braun", "Elise Roth", "Elias Neumann", "Steffen Hoffmann"];
+  showUserList = false;
+  message: string = "";
 
+  toggleUserList() {
+    this.showUserList = !this.showUserList;
+  }
+
+  closeUserList() {
+    this.showUserList = false;
+  }
+
+  bubblingProtection(event: any) {
+    event.stopPropagation();
+  }
+
+  tagUser(userToTag: string) {
+    this.message = this.message + userToTag;
+  }
 }

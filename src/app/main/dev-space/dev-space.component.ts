e31d98fa6c1f71471b83f4e752c4d80a61ec76e5
SinanDmrr <sinan-demir@hotmail.com>
2025-03-19 15:58:47 +0100

    Devspace section has been created: - Channels are loaded dynamically - Direct messages are loaded dynamically - Channels and direct messages can be collapsed and expanded - Direct messages are displayed with online status icon and name - Devspace section can be shown and hidden with the workspace menu Close / Open button

--- src/app/main/dev-space/dev-space.component.ts
+++ src/app/main/dev-space/dev-space.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dev-space',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dev-space.component.html',
-  styleUrl: './dev-space.component.scss'
+  styleUrl: './dev-space.component.scss',
 })
 export class DevSpaceComponent {
+  isMainVisible = true;
+  isChannelsExpanded = true;
+  isMessagesExpanded = true;
 
+  // Beispiel-Array für Channels
+  channels = [
+    { id: 1, name: 'general' },
+    { id: 2, name: 'development' },
+    { id: 3, name: 'design' },
+    { id: 4, name: 'off-topic' },
+  ];
+
+  // Beispiel-Array für Direktnachrichten
+  directMessages = [
+    {
+      id: 1,
+      name: 'Anna Müller',
+      online: true,
+      imgPath: '/assets/avatars/avatar_1_round.png',
+    },
+    {
+      id: 2,
+      name: 'Ben Schmidt',
+      online: false,
+      imgPath: '/assets/avatars/avatar_2_round.png',
+    },
+    {
+      id: 3,
+      name: 'Clara Weber',
+      online: true,
+      imgPath: '/assets/avatars/avatar_6_round.png',
+    },
+    {
+      id: 4,
+      name: 'David Koch',
+      online: false,
+      imgPath: '/assets/avatars/avatar_5_round.png',
+    },
+  ];
+
+  toggleMainVisibility() {
+    this.isMainVisible = !this.isMainVisible;
+  }
+
+  toggleChannels() {
+    this.isChannelsExpanded = !this.isChannelsExpanded;
+  }
+
+  toggleMessages() {
+    this.isMessagesExpanded = !this.isMessagesExpanded;
+  }
 }

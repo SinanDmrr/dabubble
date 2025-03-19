7e3eb89495617c0abd6a08485a5cb1c1d134d0c3
SasToepfer <sas.toepfer@gmail.com>
2025-03-19 09:25:48 +0100

    header layout

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -3,16 +3,18 @@ import { IChannels } from '../../interfaces/ichannels';
 import { ChannelsService } from '../../services/channels.service';
 import { UserService } from '../../services/user.service';
 import { IUser } from '../../interfaces/iuser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   dummyUser?: IUser;
+  searchQuery = "";
 
   constructor(private userService: UserService) {}
 

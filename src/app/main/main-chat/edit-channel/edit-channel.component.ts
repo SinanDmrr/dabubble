d2ed59124cf0afd95692446c5d9a72cab19ffe3d
Michael Maier <mm263@hdm-stuttgart.de>
2025-03-20 10:44:44 +0100

    edit-channed styling on edit title and description

--- src/app/main/main-chat/edit-channel/edit-channel.component.ts
+++ src/app/main/main-chat/edit-channel/edit-channel.component.ts
@@ -1,16 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-edit-channel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-channel.component.html',
   styleUrl: './edit-channel.component.scss'
 })
 export class EditChannelComponent {
   @Output() close = new EventEmitter<void>();
 
+  titleEdit: boolean = false;
+  descriptionEdit: boolean = false;
+
   closeEdit(){
     this.close.emit();
+  }
+
+  editTitle(){
+    this.titleEdit = true;
+  }
+
+  saveTitle(){
+    this.titleEdit = false;
+  }
+
+  editDescription(){
+    this.descriptionEdit = true;
+  }
+
+  saveDescription(){
+    this.descriptionEdit = false;
   }
 }

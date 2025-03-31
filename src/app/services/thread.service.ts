240fa81528c881c9a7dfea12fe26d273c7a892c5
Michael Maier <mm263@hdm-stuttgart.de>
2025-03-31 13:26:21 +0200

    service for thread added

---
+++ src/app/services/thread.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { IMessage } from '../interfaces/ichannels';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadService {
+
+  showThread: boolean = false;
+
+  private threadMessageSubject = new BehaviorSubject<IMessage>({
+    writer: "",
+    message: "",
+    answer: [],
+    time: {
+      hour: 0,
+      minute: 0,
+      day: 0,
+      month: 0,
+      year: 0,
+      dayName: "",
+      fullDate: ""
+    },
+    emojis: []
+  });
+  threadMessage$ = this.threadMessageSubject.asObservable();
+
+  private showThreadSubject = new BehaviorSubject<Boolean>(false);
+  showThread$ = this.showThreadSubject.asObservable();
+
+  constructor() { }
+
+
+  toogleThread() {
+    this.showThreadSubject.next(!this.showThread$);
+  }
+
+  setThreadMessage(message: IMessage) {
+    this.threadMessageSubject.next(message);
+  }
+}

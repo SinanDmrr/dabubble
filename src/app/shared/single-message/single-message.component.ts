ad77e91ac81f46374226ef2b50cf13341e089827
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2025-04-01 18:06:03 +0200

    thread can now be opened by clicking on answers

--- src/app/shared/single-message/single-message.component.ts
+++ src/app/shared/single-message/single-message.component.ts
@@ -1,19 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { IChannels, IEmojis, IMessage } from '../../interfaces/ichannels';
-import { UserService } from '../../services/user.service';
-import { IUser } from '../../interfaces/iuser';
-import { ChannelsService } from '../../services/channels.service';
-import { CommonModule } from '@angular/common';
-import { ThreadService } from '../../services/thread.service';
-import { FormsModule } from '@angular/forms';
-
+import { Component, Input } from "@angular/core";
+import { IChannels, IEmojis, IMessage } from "../../interfaces/ichannels";
+import { UserService } from "../../services/user.service";
+import { IUser } from "../../interfaces/iuser";
+import { ChannelsService } from "../../services/channels.service";
+import { CommonModule } from "@angular/common";
+import { ThreadService } from "../../services/thread.service";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-single-message',
+  selector: "app-single-message",
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './single-message.component.html',
-  styleUrl: './single-message.component.scss'
+  templateUrl: "./single-message.component.html",
+  styleUrl: "./single-message.component.scss",
 })
 export class SingleMessageComponent {
   @Input() message!: IMessage;
@@ -23,18 +22,22 @@ export class SingleMessageComponent {
   messageEditable: boolean = false;
   textareaMessage: string = "";
 
-  reactions: string[] = ['😂', '❤️', '👍', '🚀'];
+  reactions: string[] = ["😂", "❤️", "👍", "🚀"];
   reacted: boolean = false;
   reactedTo: string = "";
 
-  constructor(private userService: UserService, private channelService: ChannelsService, private threadService: ThreadService) {
+  constructor(
+    private userService: UserService,
+    private channelService: ChannelsService,
+    private threadService: ThreadService
+  ) {
     this.userService.getCurrentUser().subscribe((user) => {
       this.currentUser = user!;
     });
 
     this.channelService.getCurrentChannel().subscribe((channel) => {
       this.currentChannel = channel;
-    })
+    });
   }
 
   ngOnInit() {
@@ -43,7 +46,7 @@ export class SingleMessageComponent {
   }
 
   checkIfAlreadyReacted() {
-    this.message.emojis.forEach(emoji => {
+    this.message.emojis.forEach((emoji) => {
       if (emoji.username == this.currentUser.name) {
         this.reactedTo = emoji.unicode;
         this.reacted = true;
@@ -53,7 +56,7 @@ export class SingleMessageComponent {
 
   checkIfReacted(emoji: string): boolean {
     if (this.reactedTo == emoji) {
-      return true
+      return true;
     } else {
       return false;
     }
@@ -61,7 +64,7 @@ export class SingleMessageComponent {
 
   isOwnMessage(): boolean {
     if (this.message.writer == this.currentUser.name) {
-      return true
+      return true;
     } else {
       return false;
     }
@@ -71,19 +74,19 @@ export class SingleMessageComponent {
     if (this.reactedTo == newEmoji) {
       this.unclickReaction(newEmoji);
     } else {
-      if(this.reacted){
+      if (this.reacted) {
         this.unclickReaction(this.reactedTo);
-        this.react(newEmoji)
+        this.react(newEmoji);
       } else {
-        this.react(newEmoji)
+        this.react(newEmoji);
       }
     }
-    
-    this.pushToFirestore()
+
+    this.pushToFirestore();
   }
 
   unclickReaction(emojiToUnclick: string) {
-    this.message.emojis.forEach(emoji => {
+    this.message.emojis.forEach((emoji) => {
       if (emoji.unicode == emojiToUnclick) {
         this.deleteReaction(emojiToUnclick);
       }
@@ -91,7 +94,11 @@ export class SingleMessageComponent {
   }
 
   deleteReaction(emojiToDelete: string) {
-    let index = this.message.emojis.findIndex(emoji => emoji.unicode == emojiToDelete && emoji.username == this.currentUser.name);
+    let index = this.message.emojis.findIndex(
+      (emoji) =>
+        emoji.unicode == emojiToDelete &&
+        emoji.username == this.currentUser.name
+    );
     if (index !== -1) {
       this.message.emojis.splice(index, 1);
     }
@@ -100,57 +107,65 @@ export class SingleMessageComponent {
   }
 
   react(newEmoji: string) {
-    this.message.emojis.push({ unicode: newEmoji, count: 1, username: this.currentUser.name });
+    this.message.emojis.push({
+      unicode: newEmoji,
+      count: 1,
+      username: this.currentUser.name,
+    });
     this.reactedTo = newEmoji;
     this.reacted = true;
   }
 
-  getNumberOfSpecificEmoji(emoji: IEmojis): number{
+  getNumberOfSpecificEmoji(emoji: IEmojis): number {
     let count = 0;
-    this.message.emojis.forEach(emojiElement => {
-      if(emojiElement.unicode == emoji.unicode){
-        count++
+    this.message.emojis.forEach((emojiElement) => {
+      if (emojiElement.unicode == emoji.unicode) {
+        count++;
       }
     });
     return count;
   }
 
-  getUniqueEmojiArr(emojiArr: IEmojis[]){
-    let uniqueEmojis = emojiArr.filter((obj, index, array) =>
-      array.findIndex(item => item.unicode === obj.unicode) === index
-    ); 
-    
-    return uniqueEmojis
+  getUniqueEmojiArr(emojiArr: IEmojis[]) {
+    let uniqueEmojis = emojiArr.filter(
+      (obj, index, array) =>
+        array.findIndex((item) => item.unicode === obj.unicode) === index
+    );
+
+    return uniqueEmojis;
   }
 
   reactWithGivenReaction(newEmoji: string) {
-    this.message.emojis.forEach(emoji => {
+    this.message.emojis.forEach((emoji) => {
       if (emoji.unicode == newEmoji) {
         if (this.reacted) {
-          emoji.count--
+          emoji.count--;
           this.reacted = false;
         } else {
-          emoji.count++
+          emoji.count++;
           this.reacted = true;
         }
       }
     });
 
-    this.pushToFirestore()
+    this.pushToFirestore();
   }
 
   answerToMessage() {
     this.threadService.setThreadMessage(this.message);
-    this.threadService.showThreadComponent()
+    this.threadService.showThreadComponent();
   }
 
   pushToFirestore() {
     //hier muss der Channel komplett geupdatet werden (this.channelService.update()) um die Reaktionen auf die Messages zu pushen
-    this.channelService.updateChannel(this.currentChannel.id!, this.currentChannel);
+    this.channelService.updateChannel(
+      this.currentChannel.id!,
+      this.currentChannel
+    );
   }
 
   getTwoDigitNumber(number: number) {
-    return number < 10 ? '0' + number : number
+    return number < 10 ? "0" + number : number;
   }
 
   editMessage() {

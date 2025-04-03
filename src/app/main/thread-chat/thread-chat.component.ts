b64860f00b41d4f9013c9b0be85444ffff82c52b
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2025-04-03 13:59:57 +0200

    styling updated thread chat

--- src/app/main/thread-chat/thread-chat.component.ts
+++ src/app/main/thread-chat/thread-chat.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { WriteMessageComponent } from '../../shared/write-message/write-message.component';
-import { ThreadService } from '../../services/thread.service';
-import { IMessage, IChannels } from '../../interfaces/ichannels';
-import { SingleMessageComponent } from '../../shared/single-message/single-message.component';
-import { UserService } from '../../services/user.service';
-import { IUser } from '../../interfaces/iuser';
-import { ChannelsService } from '../../services/channels.service';
+import { Component, HostBinding } from "@angular/core";
+import { WriteMessageComponent } from "../../shared/write-message/write-message.component";
+import { ThreadService } from "../../services/thread.service";
+import { IMessage, IChannels } from "../../interfaces/ichannels";
+import { SingleMessageComponent } from "../../shared/single-message/single-message.component";
+import { UserService } from "../../services/user.service";
+import { IUser } from "../../interfaces/iuser";
+import { ChannelsService } from "../../services/channels.service";
 
 @Component({
-  selector: 'app-thread-chat',
+  selector: "app-thread-chat",
   standalone: true,
   imports: [WriteMessageComponent, SingleMessageComponent],
-  templateUrl: './thread-chat.component.html',
-  styleUrl: './thread-chat.component.scss',
+  templateUrl: "./thread-chat.component.html",
+  styleUrl: "./thread-chat.component.scss",
 })
 export class ThreadChatComponent {
   showThread: boolean = false;
   threadMessage: IMessage | null = null;
   currentUser: IUser | null = null;
   currentChannel!: IChannels;
+  isMainVisible = false;
 
   constructor(
     private threadService: ThreadService,
@@ -58,7 +59,10 @@ export class ThreadChatComponent {
       this.threadMessage.answer = [];
     }
     this.threadMessage.answer.push(newMessageToAdd);
-    this.channelService.updateChannel(this.currentChannel.id!, this.currentChannel);
+    this.channelService.updateChannel(
+      this.currentChannel.id!,
+      this.currentChannel
+    );
   }
 
   styleTime() {
@@ -69,8 +73,16 @@ export class ThreadChatComponent {
       day: time.getDate(),
       month: time.getMonth(),
       year: time.getFullYear(),
-      dayName: time.toLocaleDateString('de-DE', { weekday: 'long' }),
+      dayName: time.toLocaleDateString("de-DE", { weekday: "long" }),
       fullDate: time.toDateString(),
     };
+  }
+
+  toggleMainVisibility() {
+    this.isMainVisible = !this.isMainVisible;
+  }
+
+  @HostBinding('class.hidden') get isHidden() {
+    return !this.showThread; // Fügt "hidden" hinzu, wenn showThread false ist
   }
 }

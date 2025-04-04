f8236dff8a157d58e781871aac1853fd7d140227
hendrikBischoping <h.bischoping@gmx.de>
2025-04-04 23:10:33 +0200

    styled register-component + form validation + links working

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -19,6 +19,12 @@ export class RegisterComponent {
     password: ""
   }
 
+  imagePaths: Record<string, string> = {
+    person: '../../../assets/icons/person_inactive.png',
+    mail: '../../../assets/icons/mail_inactive.png',
+    lock: '../../../assets/icons/lock_inactive.png',
+  }
+
   isChecked: boolean = false;
 
   constructor(private router: Router, private userService: UserService) { }
@@ -31,6 +37,11 @@ export class RegisterComponent {
     this.router.navigateByUrl('/(login-router:legal)');
   }
 
+  goToLogin() {
+    this.router.navigate([{ outlets: { 'login-router': ['login'] } }]);
+    // this.authService.registerUserWithEmailAndPassword("Sascha", "sascha@keinelust.de", "keinelust", "assets/avatars/avatar_1.png")
+  }
+
   register(form: NgForm) {
     if (!form.valid) {
       alert("Bitte alle Felder ausfüllen!");
@@ -38,5 +49,9 @@ export class RegisterComponent {
     }
     this.userService.setUserToRegister(this.registerData.fullname, this.registerData.email, this.registerData.password);
     this.router.navigateByUrl('/(login-router:avatar)');
+  }
+
+  changeIcon(icon: string) {
+    this.imagePaths[icon] = this.imagePaths[icon] === `../../../assets/icons/${icon}_inactive.png` ? `../../../assets/icons/${icon}.png` : `../../../assets/icons/${icon}_inactive.png`;
   }
 }

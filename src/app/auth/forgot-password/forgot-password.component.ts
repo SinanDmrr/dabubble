c3029a65c3590a5b87315c354f601ff647ce013a
hendrikBischoping <h.bischoping@gmx.de>
2025-04-07 13:16:37 +0200

    'forgot-password' layout, form-validation & confirmation-popup

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
 
+  mailData = {
+    email: ""
+  }
+
+  showConfirmation: boolean = false;
+  
+  imagePaths: Record<string, string> = {
+    mail: '../../../assets/icons/mail_inactive.png',
+    send: '../../../assets/icons/send-white.png',
+  }
+  
+  constructor(private router: Router) { }
+  
+  goToLogin() {
+    this.router.navigate([{ outlets: { 'login-router': ['login'] } }]);
+    // this.authService.registerUserWithEmailAndPassword("Sascha", "sascha@keinelust.de", "keinelust", "assets/avatars/avatar_1.png")
+  }
+
+  sendMail() {
+    this.showConfirmation = true;
+    setTimeout(() => {
+      this.showConfirmation = false;
+    }, 2000);
+  }
+
+  changeIcon(icon: string) {
+    this.imagePaths[icon] = this.imagePaths[icon] === `../../../assets/icons/${icon}_inactive.png` ? `../../../assets/icons/${icon}.png` : `../../../assets/icons/${icon}_inactive.png`;
+  }
 }

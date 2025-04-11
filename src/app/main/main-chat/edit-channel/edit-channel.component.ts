571f64ac21422d34bbdf94300844e1b2ee7eb88d
Michael Maier <mm263@hdm-stuttgart.de>
2025-04-11 13:51:23 +0200

    mail to name conversion again for channel creator

--- src/app/main/main-chat/edit-channel/edit-channel.component.ts
+++ src/app/main/main-chat/edit-channel/edit-channel.component.ts
@@ -3,6 +3,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IChannels } from '../../../interfaces/ichannels';
 import { ChannelsService } from '../../../services/channels.service';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+import { IUser } from '../../../interfaces/iuser';
 
 @Component({
   selector: 'app-edit-channel',
@@ -20,9 +22,13 @@ export class EditChannelComponent {
   descriptionEdit: boolean = false;
   channelTitle = "";
   channelDescription = "";
+  userList: IUser[] = [];
 
-  constructor(private channelService: ChannelsService){
-
+  constructor(private channelService: ChannelsService, private userService: UserService,){
+    this.userService.getUserList().subscribe((userList) => {
+      this.userList = userList;
+      
+    });
   }
 
   closeEdit() {
@@ -48,8 +54,11 @@ export class EditChannelComponent {
   saveDescription() {
     this.channel.description = this.channelDescription;
     this.channelService.updateChannel(this.channel.id!, this.channel);
-    this.descriptionEdit = false;
-    
+    this.descriptionEdit = false; 
+  }
+
+  getUsernameFromMail(email: string): string {
+    return this.userList.find((user) => user.email == email)!.name || this.userList[0].name;
   }
 
   bubblingProtection(event: any) {
